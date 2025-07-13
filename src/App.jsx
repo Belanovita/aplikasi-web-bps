003f67edabb5147221b3712615d1476870a410c6
Belanovita <222212534@stis.ac.id>
2025-07-13 23:42:32 +0700

    normal lagi

--- src/App.jsx
+++ src/App.jsx
@@ -17,64 +17,53 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
-    <div
-      className="relative min-h-screen font-sans bg-cover bg-center"
-      style={{
-        backgroundImage: "url('https://ppid.bps.go.id/upload/img/kantor_bps_pusat.jpg')"
-      }}
-    >
-      {/* Overlay semi-transparent */}
-      <div className="absolute inset-0 bg-black bg-opacity-20 z-0" />
+    <div className="bg-gray-100 min-h-screen font-sans">
+      <Navbar />
+      <main className="p-4 sm:p-6 lg:p-8">
+        <Routes>
+          {/* Public Route */}
+          <Route path="/login" element={<LoginPage />} />
 
-      {/* Konten utama */}
-      <div className="relative z-10 min-h-screen">
-        <Navbar />
-        <main className="p-4 sm:p-6 lg:p-8">
-          <Routes>
-            {/* Public Route */}
-            <Route path="/login" element={<LoginPage />} />
+          {/* Protected Routes */}
+          <Route
+            path="/publications"
+            element={
+              <ProtectedRoute>
+                <PublicationListPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/publications/add"
+            element={
+              <ProtectedRoute>
+                <AddPublicationPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/publications/edit/:id"
+            element={
+              <ProtectedRoute>
+                <EditPublicationPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/publications/detail/:id"
+            element={
+              <ProtectedRoute>
+                <PublicationDetailPage />
+              </ProtectedRoute>
+            }
+          />
 
-            {/* Protected Routes */}
-            <Route
-              path="/publications"
-              element={
-                <ProtectedRoute>
-                  <PublicationListPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/publications/add"
-              element={
-                <ProtectedRoute>
-                  <AddPublicationPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/publications/edit/:id"
-              element={
-                <ProtectedRoute>
-                  <EditPublicationPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/publications/detail/:id"
-              element={
-                <ProtectedRoute>
-                  <PublicationDetailPage />
-                </ProtectedRoute>
-              }
-            />
-
-            {/* Redirect Routes */}
-            <Route path="/" element={<Navigate to="/publications" replace />} />
-            <Route path="*" element={<Navigate to="/publications" replace />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+          {/* Redirect Routes */}
+          <Route path="/" element={<Navigate to="/publications" replace />} />
+          <Route path="*" element={<Navigate to="/publications" replace />} />
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
